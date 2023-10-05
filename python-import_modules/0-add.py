84f0f5257d09aaf88cdd4da9c88aaf6e920d8480
KhairiTab <taboubikhairi68@gmail.com>
2023-10-05 15:28:20 +0100

    test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-if __name__ == "__main__":
+if (__name__ == "__main__"):
+    import add_0 as new
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    res = new.add(a, b)
+    print("{} + {} = {}".format(a, b, res))
