364ee2834fd4c18f8f7231b71ad02bdbfa004d2c
KhairiTab <taboubikhairi68@gmail.com>
2023-10-06 15:34:05 +0100

    Replace in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_b = []
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        for i in my_list
+        list_b = my_list[i]
+    list_b[idx] = element
