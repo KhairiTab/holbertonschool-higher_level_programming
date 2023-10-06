96614ff5ab93349a018334526c9ec1b8cce86f1e
KhairiTab <taboubikhairi68@gmail.com>
2023-10-06 19:39:23 +0100

    7. Tuples addition

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,7 +2,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     if len(tuple_a) < 0:
-        return 0
+        tuple_a = 0, 0
+    elif len(tuple_b) < 0:
+        tuple_b = 0, 0
     elif len(tuple_a) > 2:
         y = list(tuple_a)
         y.remove[2:]
@@ -13,7 +15,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple(y)
     else:
         return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
