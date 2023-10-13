6b9ee654dcbf9eaea1ec9a97db00bf321ffd674d
KhairiTab <taboubikhairi68@gmail.com>
2023-10-13 11:27:34 +0100

    offf

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     row = len(matrix)
-    col = len (matrix[0])
+    col = len(matrix[0])
     my_list = [[0 for _ in range(col)]for i in range(row)]
     for i in range(row):
-        for j in range (col):
+        for j in range(col):
             my_list[i][j] = matrix[i][j] * matrix[i][j]
     return(my_list)
