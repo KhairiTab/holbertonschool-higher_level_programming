915028bcb71b58f90084fe2ed35d8afeeb31275a
KhairiTab <taboubikhairi68@gmail.com>
2023-10-13 14:04:57 +0100

    update

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        update_dictionary(a_dictionary, key, value)
-    else:
-        update_dictionary(a_dictionary, key, value)
-    return a_dictionary
+    a_dictionary[key] = value
+    return (a_dictionary)
