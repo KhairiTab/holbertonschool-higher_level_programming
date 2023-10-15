c8c1e95afeb44c4af14897713b34a62138750678
KhairiTab <taboubikhairi68@gmail.com>
2023-10-15 19:20:13 +0100

    safe_print

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def safe_print_division(a, b):
     try:
-        res = a/b
+        div = a / b
     except (TypeError, ZeroDivisionError):
-        res = None
+        div = None
     finally:
-        print("Inside result:{}".format(res))
-    return(res)
+        print("Inside result: {}".format(div))
+    return (div)
