bbdc3db6109544d524f84eb0633b03a8db338b0b
KhairiTab <taboubikhairi68@gmail.com>
2023-10-19 20:25:29 +0100

    5-square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -32,10 +32,9 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
+        for i in range(0, self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+        print("")
         if self.__size == 0:
-            print("")
-        else:
-            for i in range(0, self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
             print("")
