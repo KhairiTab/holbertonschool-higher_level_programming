c6e1bbbd47b4da82b674a230191d12d7648bbe8e
KhairiTab <taboubikhairi68@gmail.com>
2023-10-24 23:32:03 +0100

    lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Defines an object"""
+
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
+    """Return a list of an object"""
     return (dir(obj))
