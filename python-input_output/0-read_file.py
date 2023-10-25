a55bde18c65d5f9ae8d90843c07080620d4d2aac
KhairiTab <taboubikhairi68@gmail.com>
2023-10-25 11:54:03 +0100

    rad_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     """Print the contents of a UTF8 text file to stdout."""
     with open(filename, mode='r', encoding="utf-8") as file:
-        for line in file:
-            print(line, end='')
+        print(file.read(), end="")
