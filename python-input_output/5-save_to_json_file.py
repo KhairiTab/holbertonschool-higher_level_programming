167d444670d92a8a2e8ea7f0f334a1cb625377ff
KhairiTab <taboubikhairi68@gmail.com>
2023-10-25 12:23:34 +0100

    save_to_json_file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,4 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write an object to a text file using JSON representation."""
-    return json.dumps(my_obj, filename)
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
