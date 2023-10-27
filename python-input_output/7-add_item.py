4d6ccb3137cfd06e2d9f2b64d4993409a4447945
KhairiTab <taboubikhairi68@gmail.com>
2023-10-27 11:12:11 +0100

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and then save them to a file"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
-if _name_ == "_main_":
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-        _import_('6-load_from_json_file').load_from_json_file
-    save_to_json_file = _import_('5-save_to_json_file').save_to_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        tab = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        tab = []
-    tab.extend(sys.argv[1:])
-    save_to_json_file(tab, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
