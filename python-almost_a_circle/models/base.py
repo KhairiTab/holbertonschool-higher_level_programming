348ae940981bf3574d5879eeb42ab25cf9e445ae
KhairiTab <135859988+KhairiTab@users.noreply.github.com>
2023-11-29 23:20:50 +0100

    base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,20 +7,21 @@ import json
 class Base:
     """Represents the base class for all polygon objects.
     """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes a new polygon object with the given id.
+        """Initialize a new Base.
 
         Args:
-            id (int): The id of this polygon object.
+            id (int): The identity of the new Base.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-            
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON serialization of a list of dicts.
