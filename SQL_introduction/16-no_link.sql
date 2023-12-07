a34af2cc216ec1479279a8c69fd35e4365b1551d
KhairiTab <taboubikhairi68@gmail.com>
2023-12-07 18:00:30 +0100

    no_link

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
---script that lists all records of the table 
--- where name is not null 
---order by descending score
+--  script that lists all records 
+-- of the table second_table
+
 SELECT score, name FROM second_table
 WHERE name != ''
 ORDER BY score DESC;
