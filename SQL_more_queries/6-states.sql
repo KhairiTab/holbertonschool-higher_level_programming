76ee3bdfefc070c4a72aebc23a00f16c1d78a803
KhairiTab <taboubikhairi68@gmail.com>
2023-12-07 23:55:17 +0100

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE table IF NOT EXISTS states(
+CREATE table IF NOT EXISTS hbtn_0d_usa.states(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
