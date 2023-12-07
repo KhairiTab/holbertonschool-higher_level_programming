6a31ca8cd4aeefb7bcf4faa8c32de5c41288b78c
KhairiTab <taboubikhairi68@gmail.com>
2023-12-07 17:20:48 +0100

    full_creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,4 +4,9 @@ CREATE TABLE IF NOT EXISTS first_table (
     name VARCHAR(256)
     score INT
 );
-INSERT INTO first_table (id, name, score) VALUES (1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
+INSERT INTO first_table (id, name, score) 
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
